16d62443b65fdfdacab002c48f0201fed353d8a9
Alain Dessi <alain.dessi@laposte.net>
2016-04-04 13:50:16 +0200

    ajout Multipart pour form::open

--- Html/HtmlBootstrapForm.php
+++ Html/HtmlBootstrapForm.php
@@ -9,88 +9,95 @@ class HtmlBootstrapForm extends Html
 
   public function __construct()
   {
-    $this->opentag = '<';
-    $this->closetag = '>';
+      $this->opentag = '<';
+      $this->closetag = '>';
   }
 
   public function BootstrapFormGroup()
   {
-    $this->prefix = "<div class=\"form-group\">";
-    $this->suffix = "</div>";
+      $this->prefix = "<div class=\"form-group\">";
+      $this->suffix = "</div>";
   }
 
   public function FormBuilder()
   {
 
-    if(!empty($this->prefix)) {
-      $html = $this->prefix;
-    }
-    else {
-      $html ='';
-    }
+      if(!empty($this->prefix)) {
+          $html = $this->prefix;
+      } else {
+          $html ='';
+      }
 
-    $html .= $this->opentag . $this->tag;
-    $html .= $this->renderAttributes();
-    $html .= $this->closetag;
+      $html .= $this->opentag . $this->tag;
+      $html .= $this->renderAttributes();
+      $html .= $this->closetag;
 
-    if(!empty($this->suffix)) {
-      $html .= $this->suffix;
-    }
+      if(!empty($this->suffix)) {
+        $html .= $this->suffix;
+      }
 
-    return $html;
+      return $html;
 
   }
 
   public function __toString()
   {
-    return $this->FormBuilder();
+      return $this->FormBuilder();
   }
 
   public function open($action=null, $method="POST")
   {
-    $this->addMethod($method);
-    $this->addAction($action);
-    $this->tag = 'form';
+      $this->addMethod($method);
+      $this->addAction($action);
+      $this->tag = 'form';
 
-    return $this;
+      return $this;
+  }
+
+  /**
+   * Ajoute enctype  = Multipart/form-data
+   * @return instance
+   */
+  public function multipart()
+  {
+      $this->setAttribute('enctype', 'multipart/form-data');
+      return $this;
   }
 
   public function close()
   {
-    return '</form>';
+      return '</form>';
   }
 
   public function label($label,$name)
   {
-    $this->opentag = "<label for=\"$name\">$label</label><";
-
-    return $this;
+      $this->opentag = "<label for=\"$name\">$label</label><";
+      return $this;
   }
 
   public function input($label,$name,$type)
   {
-    $this->BootstrapFormGroup();
+      $this->BootstrapFormGroup();
 
-    if(!is_null($label))
-    {
-        $this->label($label,$name);
-    }
+      if(!is_null($label)) {
+          $this->label($label,$name);
+      }
 
-    $this->setAttribute('type',$type);
-    $this->setAttribute('name',$name);
-    $this->setAttribute('id', $name);
-    $this->setAttribute('class', 'form-control');
+      $this->setAttribute('type',$type);
+      $this->setAttribute('name',$name);
+      $this->setAttribute('id', $name);
+      $this->setAttribute('class', 'form-control');
 
-    $this->tag = 'input';
-    $this->closetag = "/>";
+      $this->tag = 'input';
+      $this->closetag = "/>";
 
-    return $this;
+      return $this;
   }
 
   public function text($label,$name)
   {
-    $this->input($label,$name,'text');
-    return $this;
+      $this->input($label,$name,'text');
+      return $this;
   }
 
   /**
@@ -107,50 +114,51 @@ class HtmlBootstrapForm extends Html
 
   public function password($label,$name)
   {
-    $this->input($label,$name,'password');
-    return $this;
+      $this->input($label,$name,'password');
+      return $this;
   }
 
   public function date($label,$name)
   {
-    $this->input($label,$name,'date');
-    return $this;
+      $this->input($label,$name,'date');
+      return $this;
   }
 
   public function email($label,$name)
   {
-    $this->input($label,$name,'email');
-    return $this;
+      $this->input($label,$name,'email');
+      return $this;
   }
 
   public function textarea($label, $name, $rows=3)
   {
-    $this->BootstrapFormGroup();
-    $this->closetag = "></textarea>";
+      $this->BootstrapFormGroup();
+      $this->closetag = "></textarea>";
 
-    $this->label($label,$name);
+      $this->label($label,$name);
 
-    $this->setAttribute('name',$name);
-    $this->setAttribute('id', $name);
-    $this->setAttribute('class', 'form-control');
-    $this->setAttribute('rows', $rows);
+      $this->setAttribute('name',$name);
+      $this->setAttribute('id', $name);
+      $this->setAttribute('class', 'form-control');
+      $this->setAttribute('rows', $rows);
 
-    $this->tag = 'textarea';
+      $this->tag = 'textarea';
 
-    return $this;
+      return $this;
   }
 
-  public function check($label,$name) {
-    $this->prefix = '<div class="checkbox"><label>';
-    $this->suffix = ' ' . $label . '</label></div>';
+  public function check($label,$name)
+  {
+      $this->prefix = '<div class="checkbox"><label>';
+      $this->suffix = ' ' . $label . '</label></div>';
 
-    $this->setAttribute('type','checkbox');
-    $this->setAttribute('name',$name);
-    $this->setAttribute('id', $name);
+      $this->setAttribute('type','checkbox');
+      $this->setAttribute('name',$name);
+      $this->setAttribute('id', $name);
 
-    $this->tag = 'input';
+      $this->tag = 'input';
 
-    return $this;
+      return $this;
   }
 
   /**
@@ -164,28 +172,27 @@ class HtmlBootstrapForm extends Html
    */
   public function select($label,$name,$data, $defaultvalue=null)
   {
-    $this->tag = 'select';
+      $this->tag = 'select';
 
-    $this->setAttribute('name',$name);
-    $this->setAttribute('id', $name);
-    $this->setAttribute('class','form-control');
+      $this->setAttribute('name',$name);
+      $this->setAttribute('id', $name);
+      $this->setAttribute('class','form-control');
 
-    $options = '';
+      $options = '';
 
-    foreach ($data as $key => $value) {
-      if($key == $defaultvalue && !is_null($defaultvalue)) {
-        $selected = 'selected';
+      foreach ($data as $key => $value) {
+          if($key == $defaultvalue && !is_null($defaultvalue)) {
+              $selected = 'selected';
+          } else {
+              $selected = '';
+          }
+          $options .= "<option value=\"$key\"$selected>$value</option>";
       }
-      else {
-        $selected = '';
-      }
-      $options .= "<option value=\"$key\"$selected>$value</option>";
-    }
 
-    $this->prefix = "<div class=\"form-group\"><label for=\"$name\">$label</label>";
-    $this->suffix = ">" . $options . "</select></div>";
+      $this->prefix = "<div class=\"form-group\"><label for=\"$name\">$label</label>";
+      $this->suffix = ">" . $options . "</select></div>";
 
-    return $this;
+      return $this;
   }
 
   /**
@@ -196,7 +203,7 @@ class HtmlBootstrapForm extends Html
    */
   public function submit($label)
   {
-    return Button::button($label,'save','submit');
+      return Button::button($label,'save','submit');
   }
 
 } // end of class
