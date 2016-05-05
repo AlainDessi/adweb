7b4515fa64a03deb10118efd7dc169a6c0be039b
Alain Dessi <alain.dessi@laposte.net>
2016-05-05 10:22:08 +0200

    Correction et amélioration visuelle help console

--- Console/Help.php
+++ Console/Help.php
@@ -2,7 +2,7 @@
 
 namespace Core\Console;
 
-class Make extends Console
+class Help extends Console
 {
     private $type = [ 'controller',
                       'model',
@@ -14,12 +14,14 @@ class Make extends Console
      */
     public function executeCommand()
     {
+        echo $this->instanceColor->StrColor('BUSCOBON Adweb', "green") . "\n";
         echo "utilisation : adweb [command] ... [elements] \n";
         echo "Tools for buscobon framework \n";
         echo "\n";
-        echo "commands list \n";
-        foreach ($commandList as $command) {
+        echo "commands list: \n";
+        foreach ($this->commandList as $command) {
             echo "  - " . $command . "\n";
         }
+        echo "\n";
     }
 }
